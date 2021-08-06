e7ce656602a3e8c5d6b8a74a6e7557b47c80d03b
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-08-05 20:14:44 -0400

    update

--- index.js
+++ index.js
@@ -17,3 +17,32 @@ connection.connect(function (err) {
   // run the start function after the connection is made to prompt the user
   firstPrompt();
 });
+
+// function which prompts the user for what action they should take
+function firstPrompt() {
+
+    inquirer
+      .prompt({
+        type: "list",
+        name: "task",
+        message: "Would you like to do?",
+        choices: [
+          "View Employees",
+          "View Employees by Department",
+          // "View Employees by Manager",
+          "Add Employee",
+          "Remove Employees",
+          "Update Employee Role",
+          "Add Role",
+          // "Remove Role",
+          // "Update Employee Manager",
+          "End"]
+      })
+      .then(function ({ task }) {
+        switch (task) {
+          case "View Employees":
+            viewEmployee();
+            break;
+          case "View Employees by Department":
+            viewEmployeeByDepartment();
+            break;
