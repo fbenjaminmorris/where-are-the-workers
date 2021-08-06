d03bffa56b67a06f263c18e68cb4a2a0247ccc93
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-08-05 20:21:55 -0400

    update

--- index.js
+++ index.js
@@ -46,3 +46,130 @@ function firstPrompt() {
           case "View Employees by Department":
             viewEmployeeByDepartment();
             break;
+            // case "View Employees by Manager":
+        //   viewEmployeeByManager();
+        //   break;
+        case "Add Employee":
+            addEmployee();
+            break;
+          case "Remove Employees":
+            removeEmployees();
+            break;
+          case "Update Employee Role":
+            updateEmployeeRole();
+            break;
+          case "Add Role":
+            addRole();
+            break;
+          // case "Remove Role":
+          //   removeRole();
+          //   break;
+  
+          // case "Update Employee MAnager":
+          //   updateEmployeeManager();
+          //   break;
+  
+          case "End":
+            connection.end();
+            break;
+        }
+      });
+  }
+
+  //////////////////========================= 1."View Employees"/ READ all, SELECT * FROM
+
+function viewEmployee() {
+    console.log("Viewing employees\n");
+  
+    var query =
+      `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+      ON m.id = e.manager_id`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      console.table(res);
+      console.log("Employees viewed!\n");
+  
+      firstPrompt();
+    });
+    // console.log(query.sql);
+  }
+  
+  //========================================= 2."View Employees by Department" / READ by, SELECT * FROM
+  
+  // Make a department array
+  
+  function viewEmployeeByDepartment() {
+    console.log("Viewing employees by department\n");
+  
+    var query =
+      `SELECT d.id, d.name, r.salary AS budget
+    FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    GROUP BY d.id, d.name`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      // const departmentChoices = res.map(({ id, name }) => ({
+      //   name: `${id} ${name}`,
+      //   value: id
+      // }));
+  
+      const departmentChoices = res.map(data => ({
+        value: data.id, name: data.name
+      }));
+  
+      console.table(res);
+      console.log("Department view succeed!\n");
+  
+      promptDepartment(departmentChoices);
+    });
+    // console.log(query.sql);
+  }
+  
+  // User choose the department list, then employees pop up
+  
+  function promptDepartment(departmentChoices) {
+  
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "departmentId",
+          message: "Which department would you choose?",
+          choices: departmentChoices
+        }
+      ])
+      .then(function (answer) {
+        console.log("answer ", answer.departmentId);
+
+        var query =
+        `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department 
+  FROM employee e
+  JOIN role r
+	ON e.role_id = r.id
+  JOIN department d
+  ON d.id = r.department_id
+  WHERE d.id = ?`
+
+      connection.query(query, answer.departmentId, function (err, res) {
+        if (err) throw err;
+
+        console.table("response ", res);
+        console.log(res.affectedRows + "Employees are viewed!\n");
+
+        firstPrompt();
+      });
+    });
+}       
